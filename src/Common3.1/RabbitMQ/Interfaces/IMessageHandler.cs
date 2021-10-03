da0ddfefa95deeeb59413a5ee05a344e29b18bfc
Taras <rohovets.taras@gmail.com>
2021-10-03 18:15:50 +0100

    common library creation

---
+++ src/Common3.1/RabbitMQ/Interfaces/IMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.RabbitMQ
+{
+    public interface IMessageHandler
+    {
+        void Start(IMessageHandlerCallback callback);
+        void Stop();
+    }
+}
